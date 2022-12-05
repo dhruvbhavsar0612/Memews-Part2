cd41d3aa86269c3d90442a425d1729f192293c8c
dhruvbhavsar0612 <dbhavsar9898@gmail.com>
2022-12-05 15:05:12 +0530

    progress

---
+++ Navigation Bar/TopFixed/app.js
@@ -0,0 +1,18 @@
+const bars = document.querySelector('.fa-bars');
+const navbar = document.querySelector('.navbar');
+
+bars.addEventListener('click',()=>{
+    bars.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+$(window).on('scroll load',function(){
+    bars.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+
+    if($(window).scrollTop() > 30){
+        $('header').addClass('header-active');
+    }else{
+        $('header').removeClass('header-active');
+    }
+});
